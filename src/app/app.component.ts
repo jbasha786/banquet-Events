afca8f4dc902f81f051406f352b00a32f737eced
alekhyafreetime <a.revoori@freetimehospitality.com>
2024-10-16 14:58:11 +0530

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,DashboardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'VipsBanquetOffers';
+}
