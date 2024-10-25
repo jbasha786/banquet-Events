481993cb71c5676285166e6bb9cc7e35da03f8a9
alekhyafreetime <a.revoori@freetimehospitality.com>
2024-10-25 13:05:18 +0530

    login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: DashboardComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginComponent } from '../../../login/login.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private router: Router) { }
 
+  items = [
+    { src: "../../../../assets/images/logo/phone-call.png", name: "0592-356 000"  , link: ""},
+    { src: "../../../../assets/images/logo/question-sign.png",name: "Help"  , link: ""},
+    { src: "../../../../assets/images/logo/world.png" ,name: "English" , link: ""},
+    { src: "../../../../assets/images/logo/shopping-bag.png", name: "My Bookings"  , link: ""},
+    { src: "../../../../assets/images/logo/user.png" , name: "Sign In Or Join" , link: "login"},
+  ];
+  Event_Items = [
+    {name: "OverView"},
+    {name: "Best Deals"},
+    {name: "Social Events"},
+    {name: "Family Events"},
+    {name: "Corporate Events"},
+  ]
+
+  ngOnInit(): void {
+  }
+
+  redirect(name: string) {
+    this.router.navigate([name]);
+  }
 }
