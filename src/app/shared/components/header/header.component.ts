961ddb4cdc15d0ef62a5059b732cb38f8ac79105
john basha <j.shaik@freetimehospitality.com>
2024-10-25 17:37:51 +0530

    header changes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DefaultService } from '../../../services/default.service';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  @Input() headerInfo: any;
 
 }
