5665065d980f5c560e95d70af9b02ec489f45c51
alekhyafreetime <a.revoori@freetimehospitality.com>
2024-11-07 17:54:41 +0530

    booking history

--- src/app/booking-history/booking-history.component.ts
+++ src/app/booking-history/booking-history.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { MyBookingCradComponent } from '../shared/components/my-booking-crad/my-booking-crad.component';
+import { DefaultService } from '../services/default.service';
+import { bookingHistoryModel } from '../shared/_models/bookingHistoryModel';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-booking-history',
   standalone: true,
-  imports: [],
+  imports: [MyBookingCradComponent, MatButtonModule],
   templateUrl: './booking-history.component.html',
   styleUrl: './booking-history.component.scss'
 })
 export class BookingHistoryComponent {
+  bookings: bookingHistoryModel[] = [];
 
+  constructor(private defaultService: DefaultService) { }
+
+  ngOnInit() {
+    this.getInitialData();
+    console.log(this.bookings)
+  }
+
+  getInitialData() {
+    this.defaultService.getJSON().subscribe((result: any) => {
+      this.bookings = result ?.bookingHistory;
+    })
+  }
 }
--- src/app/shared/components/my-booking-crad/my-booking-crad.component.ts
+++ src/app/shared/components/my-booking-crad/my-booking-crad.component.ts
@@ -1,13 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input, Input } from '@angular/core';
+import { bookingHistoryModel } from '../../_models/bookingHistoryModel';
 
 @Component({
   selector: 'app-my-booking-crad',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-booking-crad.component.html',
   styleUrl: './my-booking-crad.component.scss'
 })
 export class MyBookingCradComponent {
   @Input() title: string | undefined;
-
+  @Input() price: any;
+  @Input() picture: any;
+  @Input() ratingPicture: any;
+  @Input() diamensions: any;
+  @Input() bookingID: any;
+  @Input() dob: any;
+  @Input() frameImg: any;
+  @Input() noOfGuestsName: any;
+  @Input() noOfGuestValue: any;
+  @Input() checkInname: any;
+  @Input() checkInvalue: any;
+  @Input() checkOutname: any;
+  @Input() checkOutvalue: any;
+  @Input() statusName: any;
+  @Input() statusvalue: any;
+  @Input() address: any;
+  @Input() rating: any;
+  @Input() viewImage: any;
+  @Input() conformationID: any;
+  @Input() cancelbooking: any;
+  @Input() editbooking: any;
+  @Input() viewdetails: any;
+  ngOnInIt() {
+  }
 }
