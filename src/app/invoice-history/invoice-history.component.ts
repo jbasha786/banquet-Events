d2a37e8fa8cb764a5745dc6a5203006818526dd5
john basha <j.shaik@freetimehospitality.com>
2024-11-07 14:30:26 +0530

    invoice history

---
+++ src/app/invoice-history/invoice-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-history.component.html',
+  styleUrl: './invoice-history.component.scss'
+})
+export class InvoiceHistoryComponent {
+
+}
---
+++ src/app/invoice-history/invoice-histoty-view/invoice-histoty-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-histoty-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-histoty-view.component.html',
+  styleUrl: './invoice-histoty-view.component.scss'
+})
+export class InvoiceHistotyViewComponent {
+
+}
