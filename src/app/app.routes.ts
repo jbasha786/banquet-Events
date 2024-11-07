8f496a8dab4f45673e3fdb58fea7ab0e0469b10c
john basha <j.shaik@freetimehospitality.com>
2024-11-07 16:30:34 +0530

    invoice view page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { OverviewComponent } from './booking/overview/overview.component';
 import { MyBookingErrorTemplateComponent } from './shared/components/my-booking-error-template/my-booking-error-template.component';
-import { ArticlesComponent } from './shared/components/articles/articles.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
--- src/app/invoice-history/invoice-histoty-view/invoice-histoty-view.component.ts
+++ src/app/invoice-history/invoice-histoty-view/invoice-histoty-view.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { DefaultService } from '../../services/default.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-invoice-histoty-view',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatDialogModule],
   templateUrl: './invoice-histoty-view.component.html',
   styleUrl: './invoice-histoty-view.component.scss'
 })
 export class InvoiceHistotyViewComponent {
+
+  InvoiceViewData: any;
+  constructor(private defaultService: DefaultService,
+    public dialog: MatDialogRef<InvoiceHistotyViewComponent>
+  ) { }
+
+  ngOnInit(): void {
+    this.getInvoiceData();
+  }
+
+  getInvoiceData() {
+    this.defaultService.getJSON().subscribe(result => {
+      this.InvoiceViewData = result.invoiceView;
+    })
+  }
+
+  close() {
+    this.dialog.close();
+  }
 
 }
