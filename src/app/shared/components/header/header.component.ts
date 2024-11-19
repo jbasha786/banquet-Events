2fbc3cf626b40060acb59f9d99e010738878ae85
priyanka4382 <p.kalidindi@vipspms.com>
2024-11-19 14:33:45 +0530

    header changes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
@@ -21,6 +21,9 @@ import { LoginComponent } from '../../../core/login/login.component';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+ 
+
+ 
 
   @Input() headerInfo: any;
   navList: headerModel[] = [];
@@ -30,12 +33,55 @@ export class HeaderComponent implements OnInit {
   constructor(private router: Router,
     private defaultService: DefaultService,
     private overlayContainer: OverlayContainer,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private renderer: Renderer2, private el: ElementRef
   ) { }
-
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.adjustLayoutForMobile();
+  }
   ngOnInit(): void {
+    this.adjustLayoutForMobile();
     this.getNavList();
     this.getnavEventList();
+  }
+  adjustLayoutForMobile() {
+    const screenWidth = window.innerWidth;
+    const leftContainer = this.el.nativeElement.querySelector('.left');
+    const rightDiv = this.el.nativeElement.querySelector('.right');
+    
+    if (screenWidth > 991) {
+      
+      if (!rightDiv) {
+        const rightDiv = this.renderer.createElement('div');
+        this.renderer.addClass(rightDiv, 'right');
+
+       
+        const twoDiv = this.el.nativeElement.querySelector('.second');
+        const threeDiv = this.el.nativeElement.querySelector('.third');
+        
+        this.renderer.appendChild(rightDiv, twoDiv);
+        this.renderer.appendChild(rightDiv, threeDiv);
+
+        
+        this.renderer.appendChild(leftContainer, rightDiv);
+      }
+    } else {
+      
+      if (rightDiv) {
+        
+        const twoDiv = this.el.nativeElement.querySelector('.second');
+        const threeDiv = this.el.nativeElement.querySelector('.third');
+
+        this.renderer.appendChild(leftContainer, twoDiv);
+        this.renderer.appendChild(leftContainer, threeDiv);
+
+        
+        this.renderer.removeChild(leftContainer, rightDiv);
+      }
+    }
+  
+
   }
   getNavList() {
     this.defaultService.getJSON().subscribe(result => {
