6ecd87b70f572d777dec71dd8cb4a233165a496b
john basha <j.shaik@freetimehospitality.com>
2024-12-18 16:37:36 +0530

    business landing page

--- src/app/business-landing-page/business-landing-page.component.ts
+++ src/app/business-landing-page/business-landing-page.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HowItWorksComponent } from './how-it-works/how-it-works.component';
 import { PlayPlannedComponent } from './play-planned/play-planned.component';
 import { WorldClassComponent } from './world-class/world-class.component';
+import { OneControlComponent } from './one-control/one-control.component';
+import { CorporateComponent } from './corporate/corporate.component';
 
 @Component({
   selector: 'app-business-landing-page',
   standalone: true,
-  imports: [HowItWorksComponent, PlayPlannedComponent, WorldClassComponent],
+  imports: [HowItWorksComponent,
+    PlayPlannedComponent,
+    WorldClassComponent,
+    OneControlComponent,
+    CorporateComponent],
   templateUrl: './business-landing-page.component.html',
   styleUrl: './business-landing-page.component.scss'
 })
