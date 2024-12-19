67b7956fb68b3a0669c6ee447a2e012b68d715ec
john basha <j.shaik@freetimehospitality.com>
2024-12-19 19:02:05 +0530

    live events

---
+++ src/app/live-events/live-events-booking/live-events-booking.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-live-events-booking',
+  standalone: true,
+  imports: [MatIconModule, CommonModule],
+  templateUrl: './live-events-booking.component.html',
+  styleUrl: './live-events-booking.component.scss'
+})
+export class LiveEventsBookingComponent {
+
+  itemQty: number = 1;
+  numberOfTickets: number = 250;
+  isbtnActive: boolean = false;
+
+  increment() {
+    this.itemQty = this.itemQty + 1;
+    this.isbtnActive = false;
+  }
+
+  decrement() {
+    this.itemQty = this.itemQty == 1 ? 1 : this.itemQty - 1;
+    this.isbtnActive = true;
+  }
+
+}
