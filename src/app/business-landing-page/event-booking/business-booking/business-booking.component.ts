4b08ba6701ead019976055dda9e4721b790293dc
john basha <j.shaik@freetimehospitality.com>
2024-12-23 12:51:33 +0530

    adding booking for business

---
+++ src/app/business-landing-page/event-booking/business-booking/business-booking.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { AboutEventComponent } from '../about-event/about-event.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-business-booking',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    AboutEventComponent,
+    CommonModule
+  ],
+  templateUrl: './business-booking.component.html',
+  styleUrl: './business-booking.component.scss'
+})
+export class BusinessBookingComponent {
+  currentStep: number = 1;
+  defaultProgressSize: number = 0;
+  progressbarWidth: any;
+  defaultPages: number = 4;
+
+  constructor(public dialogRef: MatDialogRef<BusinessBookingComponent>) {
+    this.defaultProgressSize = 100 / this.defaultPages;
+    this.progressbarWidth = this.defaultProgressSize + "%";
+  }
+
+  returnToHome() {
+    this.dialogRef.close();
+  }
+
+  back() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+    this.updatePage();
+  }
+
+  next() {
+    if (this.currentStep < this.defaultPages) {
+      this.currentStep++;
+    }
+    this.updatePage();
+  }
+
+  updatePage() {
+    this.progressbarWidth = this.defaultProgressSize * this.currentStep + "%";
+  }
+}
