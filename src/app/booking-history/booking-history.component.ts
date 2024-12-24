6507718f0fe94aa2c8d8952b86d77f1d8fb54131
alekhyafreetime <a.revoori@freetimehospitality.com>
2024-12-24 20:52:26 +0530

    new  booking history

--- src/app/booking-history/booking-history.component.ts
+++ src/app/booking-history/booking-history.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation} from '@angular/core';
+import { MatTabGroup } from '@angular/material/tabs';
 import { MyBookingCradComponent } from '../shared/components/my-booking-crad/my-booking-crad.component';
 import { DefaultService } from '../services/default.service';
 import { bookingHistoryModel } from '../shared/_models/bookingHistoryModel';
 import { InvoiceHistoryComponent } from '../invoice-history/invoice-history.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import { BookingHistoryViewComponent } from '../booking-history-view/booking-history-view.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-booking-history',
   standalone: true,
-  imports: [MatTabsModule,InvoiceHistoryComponent,BookingHistoryViewComponent],
+  imports: [MatTabsModule,InvoiceHistoryComponent,BookingHistoryViewComponent, CommonModule],
   templateUrl: './booking-history.component.html',
-  styleUrl: './booking-history.component.scss'
+  styleUrl: './booking-history.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class BookingHistoryComponent {
+  @ViewChild('tabGroup') tabGroup!: MatTabGroup;
+  showSearchBar: boolean = true;
+  activeLabel: number = 1;
   
   constructor() { }
   ngOnInit() {}
+
+  selectTab(index: any): void {
+    this.tabGroup.selectedIndex = index;
+    this.updateSearchBarVisibility(index);
+    this.activeLabel = index;
+  }
+
+  onTabChange(index: any): void {
+    this.updateSearchBarVisibility(index);
+  }
+
+  private updateSearchBarVisibility(index: any): void {
+    this.showSearchBar = index !== 1; 
+  }
 }
