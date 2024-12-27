b3c7a73b79ffd04f853b77cec896aff74ba403b1
alekhyafreetime <a.revoori@freetimehospitality.com>
2024-12-27 16:08:22 +0530

    header user image and list

--- src/app/shared/components/planner-header/planner-header.component.ts
+++ src/app/shared/components/planner-header/planner-header.component.ts
@@ -1,19 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink , Router} from '@angular/router';
 
 @Component({
   selector: 'app-planner-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink],
   templateUrl: './planner-header.component.html',
   styleUrl: './planner-header.component.scss'
 })
 export class PlannerHeaderComponent {
   notificationCount: number = 5; 
+  userName: string = "Priyanka Kalidindi";
     @Input() headerInfo: any;
-  
+    dropdownOpen: boolean = false;
+ 
+    constructor(private router: Router){}
 
   viewNotifications(): void {
     console.log('Notifications clicked!');
+  }
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+  goToHome() {
+    this.router.navigate(['']);
   }
 }
