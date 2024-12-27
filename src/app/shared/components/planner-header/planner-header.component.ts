a1f334a0c28fc7118466eee8983d652f1e179e7d
alekhyafreetime <a.revoori@freetimehospitality.com>
2024-12-27 10:31:13 +0530

    planner header initial

--- src/app/shared/components/planner-header/planner-header.component.ts
+++ src/app/shared/components/planner-header/planner-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-planner-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './planner-header.component.html',
   styleUrl: './planner-header.component.scss'
 })
