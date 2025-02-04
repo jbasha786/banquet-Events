d32a046a664e2540717f20c21d4faf1e132d098e
priyanka4382 <p.kalidindi@vipspms.com>
2025-02-05 01:40:52 +0530

    done changes

--- src/app/shared/genericComponents/date-range-picker/date-range-picker.component.ts
+++ src/app/shared/genericComponents/date-range-picker/date-range-picker.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output,ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -14,7 +14,8 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     CommonModule],
   templateUrl: './date-range-picker.component.html',
-  styleUrl: './date-range-picker.component.scss'
+  styleUrl: './date-range-picker.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class DateRangePickerComponent {
   startDate!: any;
