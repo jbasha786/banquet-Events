5c1c748d31536a0adce934f4693d8526fd16a5c1
priyanka4382 <p.kalidindi@vipspms.com>
2025-02-20 18:13:08 +0530

    accessible changes

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -56,6 +56,9 @@ export class LoginComponent {
       this.router.navigate(['home']);
     });
   }
+onguest(){
+  this.router.navigate(['/continueasguest']);
+}
 
   signUp() {
     this.router.navigate(['signUp']);
